4380cbf16ad650e85cb0a68090cc9e01d82c7d89
dongpu1813 <42663880+dongpu1813@users.noreply.github.com>
2020-12-30 07:36:17 +0800

    Delete 2020-06-03-blocks.sql

--- yiimp-peopleland-next/sql/2020-06-03-blocks.sql
+++
@@ -1,6 +0,0 @@
--- Recent additions to add after db init (.gz)
--- mysql yaamp -p < file.sql
-
- -- add blocks for solo function
-
-ALTER TABLE `blocks` ADD `solo` TINYINT(1) NULL DEFAULT NULL AFTER `category`;
